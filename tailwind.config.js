5cc11edb8c18ce970ed6b995b2bc78dff86e37a2
rafazzaky <rafiazzakymc@gmail.com>
2022-08-19 18:43:07 +0700

    update

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,22 @@
 module.exports = {
   content: ['index.html'],
   theme: {
+    screens:{
+      'sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '720px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {fontFamily:{
       pop: "'Poppins', sans-serif;",
     },
